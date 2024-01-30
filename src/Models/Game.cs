7aa1a5727b7f49ce42d0368df03c77b5d2f8376d
eryan <eryan.gauvrit@gmail.com>
2024-01-30 15:59:29 +0100

    Add models

---
+++ src/Models/Game.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src.Models
+{
+    public class Game
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public DateTime date { get; set; }
+        public Team team1 { get; set; }
+        public Team team2 { get; set; }
+        public int kills { get; set; }
+
+        public Game( int id, string name, DateTime date, Team team1, Team team2)
+        {
+            this.id = id;
+            this.name = name;
+            this.date = date;
+            this.team1 = team1;
+            this.team2 = team2;
+        }   
+    }
+}
---
+++ src/Models/Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src.Models
+{
+    public class Player
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string userName { get; set; }
+        public int gamesPlayed { get; set; }
+        public int gamesWon { get; set; }
+        public int teamId { get; set; }
+
+        public Player(int id, string name, string userName, int gamesPlayed, int gamesWon, int teamId)
+        {
+            this.id = id;
+            this.name = name;
+            this.userName = userName;
+            this.gamesPlayed = gamesPlayed;
+            this.gamesWon = gamesWon;
+            this.teamId = teamId;
+        }
+
+    }
+}
