197822999a9feca40541129de2b0808a4fdefcfc
*Stratt321963 <thomas.testaud@hotmail.fr>
2024-03-25 17:02:27 +0100

    Assign correctly all players of a team to a game

--- src/ViewModels/GamesViewModel.cs
+++ src/ViewModels/GamesViewModel.cs
@@ -25,12 +25,19 @@ namespace src.ViewModels
         {
             GameService gameService = ServiceLocator.ServiceProvider.GetService<GameService>();
             TeamService teamService = ServiceLocator.ServiceProvider.GetService<TeamService>();
+            PlayerService playerService = ServiceLocator.ServiceProvider.GetService<PlayerService>();
 
             Team team1 = teamService.AddTeam("Team 1");
             Team team2 = teamService.AddTeam("Team 2");
+            playerService.AddPlayer("Player 1", "player1", team1.id);
+            playerService.AddPlayer("Player 2", "player2", team1.id);
 
-            gameService.AddTeamGame("Game 1", DateTime.Now, team1, 10);
-            gameService.AddTeamGame("Game 2", DateTime.Now, team2, 5);
+
+            TeamWithPlayers teamWithPlayers = teamService.GetTeam(team1.id);
+            List<Player> players = teamWithPlayers.players;
+
+            gameService.AddTeamGame("Game 1", DateTime.Now, players, 10);
+            gameService.AddTeamGame("Game 2", DateTime.Now, players, 5);
 
             displayedGames = new ObservableCollection<Game>(gameService.GetGames());
             availableTeams = new ObservableCollection<Team>(teamService.GetTeams());
@@ -57,7 +64,12 @@ namespace src.ViewModels
         {
             GameService gameService = ServiceLocator.ServiceProvider.GetService<GameService>();
 
-            gameService.AddTeamGame(newGameName, newGameDate, newGameTeam, newGameScore);
+            TeamService teamService = ServiceLocator.ServiceProvider.GetService<TeamService>();
+
+            TeamWithPlayers teamWithPlayers = teamService.GetTeam(newGameTeam.id);
+            List<Player> players = teamWithPlayers.players;
+
+            gameService.AddTeamGame(newGameName, newGameDate, players, newGameScore);
 
             displayedGames = new ObservableCollection<Game>(gameService.GetGames());
 
